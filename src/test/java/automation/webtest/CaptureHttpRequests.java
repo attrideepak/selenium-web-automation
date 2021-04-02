b6d91a368be8bc93f246b0385e4c9dc4996bdb10
Aeepakattri <deepak.attri@zoomcar.com>
2021-04-02 21:07:16 +0530

    reformat code

--- src/test/java/automation/webtest/CaptureHttpRequests.java
+++ src/test/java/automation/webtest/CaptureHttpRequests.java
@@ -1,6 +1,8 @@
 package automation.webtest;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,9 +13,10 @@ import org.openqa.selenium.devtools.v89.network.Network;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
 import java.util.Optional;
 
-public class CaptureHttpRequests{
+public class CaptureHttpRequests {
     private WebDriver driver;
     DevTools devTools;
 
@@ -25,19 +28,19 @@ public class CaptureHttpRequests{
         options.addArguments("--incognito");
         driver = new ChromeDriver(options);
 
-        devTools = ((ChromeDriver)driver).getDevTools();
+        devTools = ((ChromeDriver) driver).getDevTools();
         devTools.createSession();
         devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
         devTools.addListener(Network.requestWillBeSent(),
                 entry -> {
-                    if(entry.getRequest().getUrl().contains("https://api.segment.io")) {
-                        System.out.println("Request Method : "+entry.getRequest().getMethod());
+                    if (entry.getRequest().getUrl().contains("https://api.segment.io")) {
+                        System.out.println("Request Method : " + entry.getRequest().getMethod());
                         System.out.println("Request URI : " + entry.getRequest().getUrl() + "\n");
                         System.out.println("Request headers:");
-                        entry.getRequest().getHeaders().toJson().forEach((k,v) -> System.out.println((k + ":" + v)));
-                        if(entry.getRequest().getMethod().equalsIgnoreCase("get")){
+                        entry.getRequest().getHeaders().toJson().forEach((k, v) -> System.out.println((k + ":" + v)));
+                        if (entry.getRequest().getMethod().equalsIgnoreCase("get")) {
                             System.out.println("Not request body found for get request");
-                        }else {
+                        } else {
                             System.out.println("\n" + "Request Body:");
                             String postData = entry.getRequest().getPostData().get();
                             Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -49,13 +52,13 @@ public class CaptureHttpRequests{
     }
 
     @Test
-    public void captureNetworkCalls(){
+    public void captureNetworkCalls() {
         driver.get("https://www.zoomcar.com/bangalore");
         driver.findElement(By.className("search")).click();
     }
 
     @AfterClass(alwaysRun = true)
-    public void tearDown(){
+    public void tearDown() {
         devTools.send(Network.disable());
         driver.quit();
     }
